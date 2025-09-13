228fda1eb6f43754019b46ca1b52b0926f087e47
Milan Urosevic <kobimilan995@yahoo.com>
2025-09-13 11:11:01 +0200

    refactor: Update root layout for single screen navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
 import '@/polyfills';
+import {
+    DarkTheme,
+    DefaultTheme,
+    ThemeProvider,
+} from '@react-navigation/native';
 
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -26,7 +26,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
